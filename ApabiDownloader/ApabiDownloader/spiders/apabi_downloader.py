240b2077d90269baa308d163fc944b45e002a8af
Philipp Immel <philipp.immel@gmail.com>
2024-01-19 22:20:55 +0800

    Implement recursive requests, automatic reauthentication after timeout (20 min)

--- ApabiDownloader/ApabiDownloader/spiders/apabi_downloader.py
+++ ApabiDownloader/ApabiDownloader/spiders/apabi_downloader.py
@@ -1,8 +1,8 @@
 import os.path
+import urllib.parse
+from datetime import datetime
 
 import scrapy
-import logging
-import urllib.parse
 
 
 def authentication_failed(response):
@@ -11,20 +11,24 @@ def authentication_failed(response):
         return auth_failed
 
 
-def get_image(response, page):
-    logging.info(f"Got page {page}.")
-    yield {"page": str(page), "image": response.body}
-
-
 class ApabiDownloaderSpider(scrapy.Spider):
     name = "apabi_downloader"
     allowed_domains = ["apabi.lib.pku.edu.cn"]
-    start_urls = ["http://apabi.lib.pku.edu.cn/Usp/pku/pub.mvc/?pid=login&cult=CN"]
-    var_dict = None
-    page_total = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.var_dict = None
+        self.page_total = None
+        self.downloaded_items = None
+        self.book_detail_url = "http://apabi.lib.pku.edu.cn/Usp/pku/?pid=book.detail&metaid=ISBN7-80149-306-0&cult=CN"
+        self.img_url = None
+
+    def start_requests(self):
+        start_url = "http://apabi.lib.pku.edu.cn/Usp/pku/pub.mvc/?pid=login&cult=CN"
+        yield from [scrapy.Request(url=start_url, callback=self.parse)]
 
     def parse(self, response):
-        return scrapy.FormRequest.from_response(
+        yield scrapy.FormRequest.from_response(
             response=response,
             formdata={"LoginType": "IPAutoLogin", "cult": "CN"},
             callback=self.after_login,
@@ -35,63 +39,19 @@ class ApabiDownloaderSpider(scrapy.Spider):
             self.logger.error("Login failed")
             return
         self.logger.info("Login successful.")
-        return scrapy.Request(
-            url="http://apabi.lib.pku.edu.cn/Usp/pku/?pid=book.detail&metaid=m.20151015-ZCKM-902-0064&cult=CN",
+        yield scrapy.Request(
+            url=self.book_detail_url,
             callback=self.after_book_infopage,
+            dont_filter=True
         )
 
     def after_book_infopage(self, response):
         self.logger.info("Got book info page.")
         if online_read_page := response.xpath(
-            '//a[contains(@type, "onlineread")]'
+                '//a[contains(@type, "onlineread")]'
         ).attrib["href"]:
             online_read_page = response.urljoin(online_read_page)
-            return scrapy.Request(online_read_page, callback=self.on_online_read_page)
-
-    def on_online_read_page(self, response):
-        self.logger.info("Got online read page.")
-        self.create_var_dict(response)
-        yield self.get_page_total(response)
-
-    def get_img_url(self, response):
-        self.set_page_total(response)
-        downloaded_items = []
-        for page in range(self.page_total):
-            if os.path.isfile(f"output/{str(page + 1)}.png"):
-                downloaded_items.append(page + 1)
-        # From reader.js:657; function getUrl(page)
-        # See also reader.js:2499; window.onload for value initialization
-        img_url = response.urljoin("command/imagepage.ashx")
-        self.page_total = 1
-        for current_page in range(1, self.page_total + 1):
-            if current_page in downloaded_items:
-                self.logger.info(
-                    f"Already downloaded page {str(current_page)}, skipping."
-                )
-                continue
-            self.logger.info(f"Getting page {current_page}.")
-            yield scrapy.FormRequest(
-                url=img_url,
-                method="GET",
-                formdata={
-                    "objID": self.var_dict["txtFileID"],
-                    "metaId": self.var_dict["txtMetaId"],
-                    "OrgId": self.var_dict["txtOrgIdentifier"],
-                    "scale": "1",
-                    "width": "9999",
-                    "height": "9999",
-                    "pageid": str(current_page),
-                    "ServiceType": "Imagepage",
-                    "SessionId": self.var_dict["txtSessionId"],
-                    "UserName": self.var_dict["txtuserName"],
-                    "cult": self.var_dict["txtCultureName"],
-                    "rights": self.var_dict["rights"],
-                    "time": self.var_dict["time"],
-                    "sign": self.var_dict["sign"],
-                },
-                callback=get_image,
-                cb_kwargs={"page": current_page},
-            )
+            yield scrapy.Request(online_read_page, callback=self.on_online_read_page, dont_filter=True)
 
     def create_var_dict(self, response):
         var_dict = {}
@@ -102,10 +62,15 @@ class ApabiDownloaderSpider(scrapy.Spider):
         var_dict.update(urllib.parse.parse_qsl(var_dict["urlrights"]))
         self.var_dict = var_dict
 
+    def on_online_read_page(self, response):
+        self.logger.info("Got online read page.")
+        self.create_var_dict(response)
+        yield from self.get_page_total(response)
+
     def get_page_total(self, response):
         self.logger.info("Getting page total.")
         # See reader.js:1692
-        return scrapy.FormRequest(
+        yield scrapy.FormRequest(
             url=response.urljoin("Command/Getcontent.ashx"),
             method="GET",
             formdata={
@@ -120,8 +85,78 @@ class ApabiDownloaderSpider(scrapy.Spider):
                 "UserName": self.var_dict["txtuserName"],
             },
             callback=self.get_img_url,
+            dont_filter=True
         )
 
     def set_page_total(self, response):
         self.page_total = int(response.xpath("//Content").attrib["TotalNum"])
         self.logger.info(f"Book has {self.page_total} pages.")
+
+    def get_img_url(self, response):
+        self.set_page_total(response)
+        self.downloaded_items = []
+        for page in range(self.page_total):
+            if os.path.isfile(f"output/{str(page + 1)}.png"):
+                self.downloaded_items.append(page + 1)
+        # From reader.js:657; function getUrl(page)
+        # See also reader.js:2499; window.onload for value initialization
+        self.img_url = response.urljoin("command/imagepage.ashx")
+        page = self.get_next_page_to_download(start_page=1)
+        yield from self.request_img(response, page)
+
+    def get_next_page_to_download(self, start_page):
+        page = start_page
+        while page in self.downloaded_items:
+            page += 1
+        return page
+
+    def is_timed_out(self):
+        timeout_time = datetime.strptime(self.var_dict["time"], "%Y-%m-%d %H:%M:%S")
+        time_left = timeout_time - datetime.utcnow()
+        if time_left.seconds > 30:
+            return False
+        else:
+            return True
+
+    def get_new_timeslot(self, response):
+        self.logger.info("Timeout, getting new timeslot.")
+        yield from self.after_login(response)
+
+    def request_img(self, response, page):
+        if self.is_timed_out():
+            yield from self.after_login(response)
+        else:
+            self.logger.info(f"Getting page {page}.")
+            yield scrapy.FormRequest(
+                url=self.img_url,
+                method="GET",
+                formdata={
+                    "objID": self.var_dict["txtFileID"],
+                    "metaId": self.var_dict["txtMetaId"],
+                    "OrgId": self.var_dict["txtOrgIdentifier"],
+                    "scale": "1",
+                    "width": "9999",
+                    "height": "9999",
+                    "pageid": str(page),
+                    "ServiceType": "Imagepage",
+                    "SessionId": self.var_dict["txtSessionId"],
+                    "UserName": self.var_dict["txtuserName"],
+                    "cult": self.var_dict["txtCultureName"],
+                    "rights": self.var_dict["rights"],
+                    "time": self.var_dict["time"],
+                    "sign": self.var_dict["sign"],
+                },
+                callback=self.get_image,
+                cb_kwargs={"page": page},
+                meta={"handle_httpstatus_list": [403]}
+            )
+
+    def get_image(self, response, page):
+        if response.status == 403 or self.is_timed_out() is True:
+            yield from self.get_new_timeslot(response)
+        else:
+            self.logger.info(f"Got page {page}.")
+            yield {"page": str(page), "image": response.body}
+            next_page = self.get_next_page_to_download(page + 1)
+            if next_page <= self.page_total:
+                yield from self.request_img(response, next_page)
